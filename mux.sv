dfbd7f8d8f2dbacd3cc7d4a041776e8545354fe9
vcastilla <119356332+vcastilla@users.noreply.github.com>
2025-09-30 17:51:02 +0200

    rtl: Rename MUX output wire

--- mux.sv
+++ mux.sv
@@ -4,9 +4,9 @@ module mux #(
 ) (
     input  wire [DATAWIDTH-1:0] in_i[N],
     input  wire [$clog2(N)-1:0] control_i,
-    output wire [DATAWIDTH-1:0] output_o
+    output wire [DATAWIDTH-1:0] out_o
 );
 
-    assign output_o = in_i[control_i];
+    assign out_o = in_i[control_i];
 
 endmodule
