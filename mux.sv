41a46570233690bc6a6de7706b3e2e3b2ae06610
vcastilla <119356332+vcastilla@users.noreply.github.com>
2025-09-30 11:43:00 +0200

    rtl: Add MUX

---
+++ mux.sv
@@ -0,0 +1,12 @@
+module mux #(
+    parameter integer N = 16,
+    parameter integer DATAWIDTH = 32
+) (
+    input  wire [DATAWIDTH-1:0] in_i[N],
+    input  wire [$clog2(N)-1:0] control_i,
+    output wire [DATAWIDTH-1:0] output_o
+);
+
+    assign output_o = in_i[control_i];
+
+endmodule
