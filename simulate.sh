3d19117bbc73a370bbca1bd51ac7042d7c9a62ab
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-10-03 09:33:17 +0200

    simulate: add warning flag

--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERILATORFLAGS="-Wno-fatal --timing --binary -j 0 --trace-fst --autoflush --assert -I./rtl -I./bench"
+VERILATORFLAGS="-Wall -Wno-fatal --timing --binary -j 0 --trace-fst --autoflush --assert -I./rtl -I./bench"
 
 verilator $VERILATORFLAGS $1
