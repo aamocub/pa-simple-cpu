2c1361d35f3fd57bf0b0dbf4821c8e3862ec8c27
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-10-07 09:10:35 +0200

    rtl: modify decoder to decode instructions

--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -1,14 +1,23 @@
-module decoder #(
-    parameter integer N = 16
-) (
-    input  logic [N-1:0]    in_i,
-    input  logic            enable_i,
-    output logic [2**N-1:0] out_o
+
+// Instruction Encoding - 32-bits
+// ________________________________________________________________
+// | offset[31:21] | ra[20:15] | rb[14:9] | rd[8:4] | opcode[3:0] |
+// ----------------------------------------------------------------
+
+module decoder (
+    input logic [31:0] instr_i,
+
+    output logic [31:21] offset_o,
+    output logic [20:15] ra_o,
+    output logic [ 14:9] rb_o,
+    output logic [  8:4] rd_o,
+    output logic [  3:0] opcode_o
 );
 
-    always_comb begin
-        out_o = 0;
-        out_o[in_i] = enable_i;
-    end
+    assign offset_o = instr_i[31:21];
+    assign ra_o     = instr_i[20:15];
+    assign rb_o     = instr_i[14:9];
+    assign rd_o     = instr_i[8:4];
+    assign opcode_o = instr_i[3:0];
 
 endmodule
