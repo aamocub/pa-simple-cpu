12a9699328156798d7c5db563df8e598833c2eaf
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-10-29 01:29:44 +0100

    rtl: add flush signal to register module

--- rtl/register.sv
+++ rtl/register.sv
@@ -1,15 +1,18 @@
 module register #(
     parameter integer DATAWIDTH = 32
 ) (
-    input  wire                 clk_i,
-    input  wire                 rst_i,
-    input  wire                 en_i,
-    input  reg  [DATAWIDTH-1:0] d_i,
-    output reg  [DATAWIDTH-1:0] q_o
+    input  logic                 clk_i,
+    input  logic                 rst_i,
+    input  logic                 en_i,
+    input  logic                 flush_i,
+    input  logic [DATAWIDTH-1:0] d_i,
+    output logic [DATAWIDTH-1:0] q_o
 );
 
-    always_ff @(posedge clk_i or posedge rst_i) begin
+    always_ff @(posedge clk_i, posedge rst_i) begin
         if (rst_i) begin
+            q_o <= '0;
+        end else if (flush_i) begin
             q_o <= '0;
         end else if (en_i) begin
             q_o <= d_i;
