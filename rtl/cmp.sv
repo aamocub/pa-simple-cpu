ad40679a2d703d090d4483a3177c24defe6a0727
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-11-09 14:16:26 +0100

    rtl: rework cmp module to work with riscv instructions

--- rtl/cmp.sv
+++ rtl/cmp.sv
@@ -1,16 +1,22 @@
-module cmp #(
-    parameter integer DATAWIDTH = 32
-) (
-    input wire [DATAWIDTH-1:0] a_i,
-    input wire [DATAWIDTH-1:0] b_i,
-    input wire [1:0] op_i,
-    output wire out_o
+module cmp
+    import pa_pkg::*;
+    import riscv_pkg::*;
+(
+    input logic [XLEN-1:0] a_i,
+    input logic [XLEN-1:0] b_i,
+    input instr_op_t op_i,
+    output logic out_o
 );
 
-    assign out_o = (op_i == 2'b00) ? 0 :
-                   (op_i == 2'b01) ? a_i == b_i :
-                   (op_i == 2'b10) ? a_i >  b_i :
-                   (op_i == 2'b11) ? a_i >= b_i :
-                    'x;
-
+    always_comb begin
+        case (op_i)
+            BEQ: out_o = ($signed(a_i) == $signed(b_i)) ? 1 : 0;
+            BNE: out_o = ($signed(a_i) != $signed(b_i)) ? 1 : 0;
+            BLT: out_o = ($signed(a_i) < $signed(b_i)) ? 1 : 0;
+            BGE: out_o = ($signed(a_i) >= $signed(b_i)) ? 1 : 0;
+            BLTU: out_o = (a_i < b_i) ? 1 : 0;
+            BGEU: out_o = (a_i >= b_i) ? 1 : 0;
+            default: out_o = 0;
+        endcase
+    end
 endmodule
