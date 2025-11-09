906bfc5531162082a9f0ab6a14fd34ebf1947450
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-11-09 14:16:56 +0100

    rtl: add execution stage module

---
+++ rtl/ex_stage.sv
@@ -0,0 +1,33 @@
+module ex_stage
+    import pa_pkg::*;
+    import riscv_pkg::*;
+#(
+) (
+    input logic clk_i,
+    input logic rst_i,
+    input id_stage_t id_i,
+    output ex_stage_t ex_o
+);
+    always_comb begin
+        ex_o.is_wb    = id_i.is_wb;
+        ex_o.is_ld    = id_i.is_ld;
+        ex_o.is_st    = id_i.is_st;
+        ex_o.uses_rs2 = id_i.uses_rs2;
+        ex_o.data_rs2 = id_i.data_rs2;
+    end
+
+    cmp cmp (
+        .a_i  (id_i.data_rs1),
+        .b_i  (id_i.data_rs2),
+        .op_i (id_i.op),
+        .out_o(ex_o.cmp_result)
+    );
+
+    alu alu (
+        .a_i     (id_i.data_rs1),
+        .b_i     (id_i.uses_rs2 ? id_i.data_rs2 : id_i.imm),
+        .opcode_i(id_i.op),
+        .out_o   (ex_o.alu_result)
+    );
+
+endmodule
