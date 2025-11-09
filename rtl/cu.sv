9f62c14223f22bd3fbbb238da5357fbbf409f6a7
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-11-09 14:16:39 +0100

    rtl: add control unit module skeleton

---
+++ rtl/cu.sv
@@ -0,0 +1,13 @@
+module cu
+    import pa_pkg::*;
+    import riscv_pkg::*;
+(
+    input if_stage_t if_i,
+    input id_stage_t id_i,
+    input ex_stage_t ex_i
+);
+    always_comb begin
+
+    end
+
+endmodule
