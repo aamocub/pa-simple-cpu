0f45a49682e313c407730404884f7720f63408cb
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-11-18 12:09:42 +0100

    rtl: refactor and update IF stage to take into account stall and flush signals.
    Updated the module, left the old one as `if_stage_old` incase the current module has some unknown issue.

--- rtl/if_stage.sv
+++ rtl/if_stage.sv
@@ -1,12 +1,3 @@
-//// IF stage state machine
-// 1. Generates new PC
-// 2. Requests memory for instruction in PC
-// 3. Waits for result
-// 4. Sends result to if_o
-// 5. Go to step 1
-
-// TODO: make this a 1-cycle stage or not
-
 module if_stage
     import riscv_pkg::*;
     import pa_pkg::*;
@@ -19,69 +10,74 @@ module if_stage
     output if_stage_t if_o     // Output data from the IF stage
 );
     // verilog_format: off
-    enum { RST, IDLE, PC_GEN, PC_FETCH } state;
+    enum { RST, IF1, IF2 } state;
     // verilog_format: on
-
     logic [PHY_ADDR_LEN-1:0] pc;
     logic [PHY_ADDR_LEN-1:0] next_pc;
+    instruction_t instr, next_instr;
 
-    always_comb begin
-        if_o.pc = pc;
+    always_ff @(posedge clk_i, posedge rst_i) begin : transitions_block
         if (rst_i) begin
-            if_o.instr = NOP_INSTR;
-            next_pc = pc;
+            pc <= next_pc;
+            // instr <= next_instr;
+            state <= RST;
         end else begin
-            priority case (state)
-                RST: ;
-                IDLE: begin
-                    req_o.addr  = pc;
-                    req_o.valid = 1;
-                end
-                PC_GEN: begin
-                    case (ctrl_i.taken)
-                        0: next_pc = pc + 4;
-                        1: next_pc = ctrl_i.addr;
-                    endcase
-                    req_o.addr  = next_pc;
-                    req_o.valid = 1;
-                    if (resp_i.valid) begin
-                        if_o.instr = resp_i.data;
-                    end
-                end
-                PC_FETCH: begin
-                    req_o.valid = 0;
-                    if (resp_i.valid) begin
-                        if_o.instr = resp_i.data;
-                        case (ctrl_i.taken)
-                            0: next_pc = pc + 4;
-                            1: next_pc = ctrl_i.addr;
-                        endcase
-                        req_o.addr  = next_pc;
-                        req_o.valid = 1;
-                    end else begin
-                        if_o.instr = NOP_INSTR;
-                    end
-                end
+            pc <= next_pc;
+            // instr <= next_instr;
+            unique case (state)
+                RST: state <= IF1;
+                IF1: state <= IF2;
+                IF2: state <= IF2;
             endcase
-            if (ctrl_i.flush) begin
-                if_o.instr = NOP_INSTR;
-            end
         end
     end
 
-    always_ff @(posedge clk_i, posedge rst_i) begin
-        if (rst_i) begin
-            state <= RST;
-            pc <= PC_RESET_ADDR;
+    always_comb begin : if_out
+        if (ctrl_i.flush) begin
+            if_o.pc = 0;
+            if_o.instr = NOP_INSTR;
+        end else if (ctrl_i.stall) begin
+            if_o.pc = 0;
+            if_o.instr = NOP_INSTR;
         end else begin
-            pc <= next_pc;
+            if_o.pc = pc;
+            if_o.instr = instr;
+        end
+    end
+    always_comb begin : generate_new_pc
+        if (rst_i) begin
+            next_pc = PC_RESET_ADDR;
+        end else if (ctrl_i.stall) begin
+            next_pc = pc;
+        end else if (state == RST || state == IF1) begin
+            next_pc = pc;
+        end else begin
+            case (ctrl_i.taken)
+                0: next_pc = pc + 4;
+                1: next_pc = ctrl_i.addr;
+                default: next_pc = pc + 4;
+            endcase
+        end
+    end
+    always_comb begin : fetch_pc_from_mem
+        if (rst_i) begin
+            req_o.valid = 0;
+            instr = NOP_INSTR;
+        end else begin
+            instr = instr;
             unique case (state)
-                RST: state <= IDLE;
-                IDLE, PC_GEN: begin
-                    state <= PC_FETCH;
+                RST: ;
+                IF1: begin
+                    req_o.valid = 1;
+                    req_o.addr  = next_pc;
                 end
-                PC_FETCH: begin
-                    state <= resp_i.valid ? PC_GEN : state;
+                IF2: begin
+                    req_o.valid = 0;
+                    if (resp_i.valid) begin
+                        instr = resp_i.data;
+                        req_o.valid = 1;
+                        req_o.addr = next_pc;
+                    end
                 end
             endcase
         end
