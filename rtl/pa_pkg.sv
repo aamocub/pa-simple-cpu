a0cee0d8412bf6e2338cdcf4a5d52589ca1f0dd9
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-12-14 19:36:23 +0100

    rtl: minor comment changes

--- rtl/pa_pkg.sv
+++ rtl/pa_pkg.sv
@@ -1,109 +1,14 @@
-// This package includes all parameters, types, enums and others that are required by the processor itself.
-
 package pa_pkg;
     import riscv_pkg::*;
 
-
-    /* Memory parameters*/
-    localparam MEM_ACCESS_DELAY = 1;  // How many cycles does it take the memory to access data
-
-    /* Core definitions */
-
-    localparam PHY_ADDR_LEN = 32;  // Bit width of physical address
-    // localparam PC_RESET_ADDR = 32'h1000;
-    localparam PC_RESET_ADDR = 32'h0000;
-    localparam PC_EXCEPTION_ADDR = 32'h8000;
-    localparam RF_NUMREGS = 32;
-
+    /* --------------------------------------------- Memory definitions --------------------------------------------- */
     // verilog_format: off
-    typedef enum {
-        NOP, LUI, AUIPC, JAL, JALR, BEQ, BNE, BLT, BGE, BLTU, BGEU, LB, LH, LW, LBU, LHU, SB, SH, SW, ADDI, SLTI, SLTIU,
-        XORI, ORI, ANDI, SLLI, SRLI, SRAI, ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND, ECALL, EBREAK, MUL, MULH,
-        MULHSU, MULHU, DIV, DIVU, REM, REMU, ILLEGAL
-    } instr_op_t;
-    typedef enum logic [2:0] {BYTE, UBYTE, HALF, UHALF, WORD} mem_width_t;
+    localparam MEM_ACCESS_DELAY = 1;  // How many cycles does it take the memory to access data
+    localparam PHY_ADDR_LEN     = 32; // Bit width of physical address
+    typedef enum logic [2:0] { BYTE, UBYTE, HALF, UHALF, WORD } mem_width_t; // Size/width of memory access
     // verilog_format: on
 
-    /* Exception Vector*/
-    typedef struct packed {
-        logic ill_instr;
-        logic div_by_zero;
-    } except_t;
-
-    /* Control unit types */
-
-    typedef struct packed {
-        logic                    taken;
-        logic [PHY_ADDR_LEN-1:0] addr;
-        logic                    stall;
-        logic                    flush;
-        logic                    except_valid;
-        logic [PHY_ADDR_LEN-1:0] except_addr;
-    } cu_if_t;
-    typedef struct packed {
-        logic stall;
-        logic flush;
-    } cu_id_t;
-    typedef struct packed {
-        logic stall;
-        logic flush;
-        logic [1:0] alu_mux_a_sel;
-        logic [1:0] alu_mux_b_sel;
-        logic [1:0] cmp_mux_a_sel;
-        logic [1:0] cmp_mux_b_sel;
-    } cu_ex_t;
-    typedef struct packed {
-        logic stall;
-        logic flush;
-    } cu_mm_t;
-    typedef struct packed {
-        logic stall;
-        logic flush;
-    } cu_wb_t;
-
-    /* Module types */
-
-    typedef struct packed {
-        instruction_t    instr;      // Instruction
-        logic [XLEN-1:0] pc;         // Current PC
-        except_t         excep_vec;  // Exception vector
-    } if_stage_t;
-
-    typedef struct packed {
-        logic [4:0]      rs1;        // Source register 1
-        logic [4:0]      rs2;        // Source register 2
-        logic [4:0]      rd;         // Destination register
-        logic [XLEN-1:0] data_rs1;   // Value of register 1
-        logic [XLEN-1:0] data_rs2;   // Value of register 2
-        logic            is_wb;      // Is it going to write to regfile
-        logic            is_ld;      // Is it a load
-        logic            is_st;      // Is it a store
-        logic            is_br;      // Is it a jump/branch
-        logic            uses_rs2;   // Does the instruction use rs2
-        logic [XLEN-1:0] imm;        // Immediate
-        logic [XLEN-1:0] pc;         // Current PC
-        mem_width_t      mem_width;  // Width of memory access
-        instr_op_t       op;         // Operation to perform
-        except_t         excep_vec;  // Exception vector
-    } id_stage_t;
-
-    typedef struct packed {
-        logic [4:0]      rs1;         // Source register 1
-        logic [4:0]      rs2;         // Source register 2
-        logic [4:0]      rd;          // Destination register
-        logic            is_wb;       // Is it going to write to regfile
-        logic [XLEN-1:0] alu_result;
-        logic            is_taken;    // Is branch taken
-        logic            is_ld;       // Is it a load
-        logic            is_st;       // Is it a store
-        logic            uses_rs2;    // Does the instruction use rs2
-        logic [XLEN-1:0] data_rs2;    // Value of register 2
-        mem_width_t      mem_width;   // Width of memory access
-        logic            do_stall;    // Should previous instr be stalled
-        except_t         excep_vec;   // Exception vector
-    } ex_stage_t;
-
-    /* Memory arbitrer */
+    /* ----------------------------------------- Memory arbitrer definitions ---------------------------------------- */
     typedef struct packed {
         logic                    valid;
         logic [PHY_ADDR_LEN-1:0] addr;
@@ -127,6 +32,98 @@ package pa_pkg;
     } mm_write_req_t;
     typedef struct packed {logic valid;} mm_write_resp_t;
 
+
+    /* ------------------------------------------------- Exceptions ------------------------------------------------- */
+    localparam PC_EXCEPTION_ADDR = 32'h8000;
+    typedef struct packed {
+        logic ill_instr;
+        logic div_by_zero;
+    } exception_t;
+
+    /* ---------------------------------------------- Core definitions ---------------------------------------------- */
+    localparam PC_RESET_ADDR = 32'h0000;
+    localparam RF_NUMREGS = 32;
+
+    /* --------------------------------------------- Decode definitions --------------------------------------------- */
+    // verilog_format: off
+    typedef enum {
+        NOP, LUI, AUIPC, JAL, JALR, BEQ, BNE, BLT, BGE, BLTU, BGEU, LB, LH, LW, LBU, LHU, SB, SH, SW, ADDI, SLTI, SLTIU,
+        XORI, ORI, ANDI, SLLI, SRLI, SRAI, ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND, ECALL, EBREAK, MUL, MULH,
+        MULHSU, MULHU, DIV, DIVU, REM, REMU, ILLEGAL
+    } instr_op_t;
+    // verilog_format: on
+
+    /* --------------------------------------------- Control definitions -------------------------------------------- */
+    typedef struct packed {
+        logic [1:0]              pcsel;         // PC select
+        logic [PHY_ADDR_LEN-1:0] addr;          // Address to jump to
+        logic                    stall;         // Stall stage
+        logic                    flush;         // Flush stage
+        logic                    except_valid;  // Is there a valid exception
+        logic [PHY_ADDR_LEN-1:0] except_addr;   // Where to jump for the exception
+    } cu_if_t;
+    typedef struct packed {
+        logic stall;  // Stall stage
+        logic flush;  // Flush stage
+    } cu_id_t;
+    typedef struct packed {
+        logic       stall;          // Stall stage
+        logic       flush;          // Flush stage
+        logic [1:0] alu_mux_a_sel;  // ALU reg A mux select
+        logic [1:0] alu_mux_b_sel;  // ALU reg B Mux select
+        logic [1:0] cmp_mux_a_sel;  // CMP reg A Mux select
+        logic [1:0] cmp_mux_b_sel;  // CMP reg B Mux select
+    } cu_ex_t;
+    typedef struct packed {
+        logic stall;  // Stall stage
+        logic flush;  // Flush stage
+    } cu_mm_t;
+    typedef struct packed {
+        logic stall;  // Stall stage
+        logic flush;  // Flush stage
+    } cu_wb_t;
+
+    /* ---------------------------------------------- Stage definitions --------------------------------------------- */
+    typedef struct packed {
+        instruction_t    instr;      // Instruction
+        logic [XLEN-1:0] pc;         // Current PC
+        exception_t      excep_vec;  // Exception vector
+    } if_stage_t;
+
+    typedef struct packed {
+        logic [4:0]      rs1;        // Source register 1
+        logic [4:0]      rs2;        // Source register 2
+        logic [4:0]      rd;         // Destination register
+        logic [XLEN-1:0] data_rs1;   // Value of register 1
+        logic [XLEN-1:0] data_rs2;   // Value of register 2
+        logic            is_wb;      // Is it going to write to regfile
+        logic            is_ld;      // Is it a load
+        logic            is_st;      // Is it a store
+        logic            is_br;      // Is it a jump/branch
+        logic            uses_rs2;   // Does the instruction use rs2
+        logic [XLEN-1:0] imm;        // Immediate
+        logic [XLEN-1:0] pc;         // Current PC
+        mem_width_t      mem_width;  // Width of memory access
+        instr_op_t       op;         // Operation to perform
+        exception_t      excep_vec;  // Exception vector
+    } id_stage_t;
+
+    typedef struct packed {
+        logic [4:0]      rs1;         // Source register 1
+        logic [4:0]      rs2;         // Source register 2
+        logic [4:0]      rd;          // Destination register
+        logic            is_wb;       // Is it going to write to regfile
+        logic [XLEN-1:0] alu_result;
+        logic            is_taken;    // Is branch taken
+        logic            is_ld;       // Is it a load
+        logic            is_st;       // Is it a store
+        logic            uses_rs2;    // Does the instruction use rs2
+        logic [XLEN-1:0] data_rs2;    // Value of register 2
+        mem_width_t      mem_width;   // Width of memory access
+        logic            do_stall;    // Should previous instr be stalled
+        exception_t      excep_vec;   // Exception vector
+    } ex_stage_t;
+
     typedef struct packed {
         logic [XLEN-1:0] data;
         logic [XLEN-1:0] data_rs2;   // Value of register 2
@@ -135,14 +132,14 @@ package pa_pkg;
         logic [4:0]      rs1;        // Source register 1
         logic [4:0]      rs2;        // Source register 2
         logic [4:0]      rd;         // Destination register
-        except_t         excep_vec;  // Exception vector
+        exception_t      excep_vec;  // Exception vector
     } mm_stage_t;
 
     typedef struct packed {
         logic            is_wb;
         logic [4:0]      rd;
         logic [XLEN-1:0] data;
-        except_t         excep_vec;  // Exception vector
+        exception_t      excep_vec;  // Exception vector
     } wb_stage_t;  // from WB to ID stage
 
     // Memory and Memory Arbitrer structs
