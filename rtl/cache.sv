c940780387b1d681b1771917e73d73509753a052
Alfonso Amorós Cubells <aamocub@gmail.com>
2025-12-30 21:51:38 +0100

    rtl: cache module refactor

--- rtl/cache.sv
+++ rtl/cache.sv
@@ -1,82 +1,114 @@
-// - Directly mapped
-// - Write-back
-// - Write-allocate
-
 module cache
     import riscv_pkg::*;
     import pa_pkg::*;
 #(
-    parameter integer unsigned LINE_LEN = CACHE_LINE_LEN,
-    localparam integer unsigned NUM_SETS = 4,
-    localparam integer unsigned M = $clog2(LINE_LEN / 8),
-    localparam integer unsigned N = $clog2(NUM_SETS) + M
+    parameter integer unsigned LINE_LEN   = CACHE_LINE_LEN,
+    parameter integer unsigned ADDR_WIDTH = PHY_ADDR_LEN
 ) (
     input logic clk_i,
     input logic rst_i,
-    memory_intf.SV stage_io,
+    memory_intf.SV core_io,
     memory_intf.CL mem_io
 );
 
-    typedef enum {
-        IDLE,
-        MISS,
-        WRITEBACK,
-        WAIT,
-        WRITE
-    } state_t;
+    localparam integer unsigned NUM_SETS = 4;
+    localparam integer unsigned M = $clog2(LINE_LEN / 8);
+    localparam integer unsigned N = $clog2(NUM_SETS) + M;
+
     typedef struct packed {
-        logic valid;
-        logic dirty;
-        logic [PHY_ADDR_LEN-N-1:0] tag;
-        logic [LINE_LEN-1:0] data;
-    } line_t;
-    typedef struct packed {
-        logic valid;
-        logic write_en;
-        mem_width_t kind;
-        logic [PHY_ADDR_LEN-1:0] addr;
-        logic [XLEN-1:0] data;
-    } pending_req_t;
+        logic                  valid, write;
+        logic [ADDR_WIDTH-1:0] addr;
+        logic [XLEN-1:0]       data;
+        mem_width_t            kind;
+    } req_t;
 
-    state_t state;
-    line_t line[NUM_SETS];
-    logic is_req_in_cache;
-    pending_req_t pending_read, pending_write;
+    // verilog_format: off
+    enum { IDLE, MISS, POSTMISS, WRITEBACK, WRITE } state;
+    // verilog_format: on
 
-    wire [PHY_ADDR_LEN-N-1:0] tag;
-    wire [N-M-1:0] idx;
-    wire [M-1:0] offset;
-    assign tag = stage_io.req_addr[PHY_ADDR_LEN-1:N];
-    assign idx = stage_io.req_addr[N-1:M];
-    assign offset = stage_io.req_addr[M-1:0];
+    req_t pending_req, next_req;
+    wire [ADDR_WIDTH-N-1:0] tag = pending_req.addr[ADDR_WIDTH-1:N];
+    wire [N-M-1:0] idx = pending_req.addr[N-1:M];
+    wire [M-1:0] offset = pending_req.addr[M-1:0];
 
-    assign is_req_in_cache = stage_io.req_valid && line[idx].valid && tag == line[idx].tag;
+    logic [ADDR_WIDTH-1:0] addr_to_cache;
+    logic write_to_cache;
+    logic write_line_to_cache;
+    logic [LINE_LEN-1:0] write_data_to_cache;
+    mem_width_t width;
+    logic [XLEN-1:0] data_from_cache;
+    logic is_hit, is_dirty;
 
-    always_ff @(posedge clk_i, posedge rst_i) begin : transitions
+    cache_data #(
+        .LINE_LEN  (LINE_LEN  /* default CACHE_LINE_LEN */),
+        .NUM_SETS  (NUM_SETS  /* default 4 */),
+        .ADDR_WIDTH(ADDR_WIDTH  /* default PHY_ADDR_LEN */)
+    ) cache_data (
+        .clk_i       (clk_i),
+        .rst_i       (rst_i),
+        .addr_i      (addr_to_cache),
+        .write_i     (write_to_cache),
+        .write_line_i(write_line_to_cache),
+        .write_data_i(write_data_to_cache),
+        .kind_i      (width),
+        .hit_o       (is_hit),
+        .dirty_o     (is_dirty),
+        .data_o      (data_from_cache)
+    );
+
+    always_ff @(posedge clk_i, posedge rst_i) begin
         if (rst_i) begin
             state <= IDLE;
+            pending_req <= '0;
         end else begin
             unique case (state)
                 IDLE: begin
-                    state <= (stage_io.req_valid && !is_req_in_cache) ? MISS : state;
+                    if (core_io.req_valid) begin
+                        pending_req.valid <= 1;
+                        pending_req.write <= core_io.req_write_en;
+                        pending_req.addr  <= core_io.req_addr;
+                        pending_req.data  <= core_io.req_data;
+                        pending_req.kind  <= core_io.req_type;
+                        if (!is_hit) begin
+                            state <= is_dirty ? WRITEBACK : MISS;
+                        end else begin
+                            state <= IDLE;
+                        end
+                    end
                 end
                 MISS: begin
-                    if (line[idx].valid && line[idx].dirty) begin
-                        state <= WRITEBACK;
+                    if (mem_io.resp_valid) begin
+                        state <= pending_req.write ? WRITE : core_io.req_valid ? POSTMISS : IDLE;
+                        if (pending_req.write) begin
+                            state <= WRITE;
+                        end else if (core_io.req_valid) begin
+                            next_req.valid <= 1;
+                            next_req.write <= core_io.req_write_en;
+                            next_req.addr <= core_io.req_addr;
+                            next_req.data <= core_io.req_data;
+                            next_req.kind <= core_io.req_type;
+                            state <= POSTMISS;
+                        end else begin
+                            state <= IDLE;
+                        end
+                    end
+                end
+                POSTMISS: begin
+                    if (!is_hit) begin
+                        state <= is_dirty ? WRITEBACK : MISS;
+                    end else if (core_io.req_valid) begin
+                        next_req.valid <= 1;
+                        next_req.write <= core_io.req_write_en;
+                        next_req.addr <= core_io.req_addr;
+                        next_req.data <= core_io.req_data;
+                        next_req.kind <= core_io.req_type;
+                        state <= POSTMISS;
                     end else begin
-                        state <= WAIT;
+                        state <= IDLE;
                     end
                 end
                 WRITEBACK: begin
-                    state <= WAIT;
-                end
-                WAIT: begin
-                    if (mem_io.resp_valid) begin
-                        if (pending_write.valid) state <= WRITE;
-                        else state <= IDLE;
-                    end else begin
-                        state <= state;
-                    end
+                    state <= MISS;
                 end
                 WRITE: begin
                     state <= IDLE;
@@ -85,100 +117,95 @@ module cache
         end
     end
 
-    always_ff @(posedge clk_i, posedge rst_i) begin
-        if (rst_i) begin
-            foreach (line[i]) line[i] <= '0;
-        end else begin
-            stage_io.resp_valid <= 0;
-            mem_io.req_valid <= '0;
-            mem_io.req_addr <= '0;
-            mem_io.req_data <= '0;
-            mem_io.req_write_en <= '0;
-            unique case (state)
-                IDLE: begin
-                    pending_read  <= '0;
-                    pending_write <= '0;
-                    if (is_req_in_cache) begin
-                        stage_io.resp_valid <= 1;
-                        unique case (stage_io.req_write_en)
-                            0: begin  // READ
-                                unique case (stage_io.req_type)
-                                    WORD: stage_io.resp_data <= line[idx].data[offset*8+:32];
-                                    HALF, UHALF:
-                                    stage_io.resp_data <= {16'b0, line[idx].data[offset*8+:16]};
-                                    BYTE, UBYTE:
-                                    stage_io.resp_data <= {24'b0, line[idx].data[offset*8+:8]};
-                                endcase
-                            end
-                            1: begin  // WRITE
-                                line[idx].dirty <= 1;
-                                unique case (stage_io.req_type)
-                                    WORD: line[idx].data[offset*8+:32] <= stage_io.req_data;
-                                    HALF, UHALF:
-                                    line[idx].data[offset*8+:16] <= stage_io.req_data[15:0];
-                                    BYTE, UBYTE:
-                                    line[idx].data[offset*8+:8] <= stage_io.req_data[7:0];
-                                endcase
-                            end
+    always_comb begin
+        core_io.resp_valid = 0;
+        core_io.resp_data = '0;
+        addr_to_cache = '0;
+        write_to_cache = '0;
+        write_data_to_cache = '0;
+        write_line_to_cache = 0;
+        mem_io.req_valid = '0;
+        mem_io.req_addr = '0;
+        mem_io.req_data = '0;
+        mem_io.req_write_en = '0;
+        mem_io.req_type = BYTE;
+        width = core_io.req_type;
+        unique case (state)
+            IDLE: begin
+                addr_to_cache = core_io.req_addr;
+                if (core_io.req_valid) begin
+                    if (is_hit) begin
+                        core_io.resp_valid = 1;
+                        core_io.resp_data  = data_from_cache;
+                        if (core_io.req_write_en) begin
+                            write_to_cache = 1;
+                            write_data_to_cache = {96'b0, core_io.req_data};
+                        end
+                    end else begin
+                        if (is_dirty) begin : WRITEBACK_LINE
+                            mem_io.req_valid = 1;
+                            mem_io.req_addr = {core_io.req_addr[ADDR_WIDTH-1:M], {M{1'b0}}};
+                            mem_io.req_write_en = 1;
+                            mem_io.req_data = data_from_cache;
+                        end else begin : GET_LINE
+                            mem_io.req_valid = 1;
+                            mem_io.req_addr = {core_io.req_addr[ADDR_WIDTH-1:M], {M{1'b0}}};
+                            mem_io.req_write_en = 0;
+                        end
+                    end
+                end
+            end
+            MISS: begin
+                addr_to_cache = pending_req.addr;
+                if (mem_io.resp_valid) begin
+                    write_data_to_cache = mem_io.resp_data;
+                    write_line_to_cache = 1;
+                    if (!pending_req.write) begin
+                        core_io.resp_valid = 1;
+                        unique case (pending_req.kind)
+                            WORD: core_io.resp_data = mem_io.resp_data[offset*8+:32];
+                            HALF, UHALF:
+                            core_io.resp_data = {16'b0, mem_io.resp_data[offset*8+:16]};
+                            BYTE, UBYTE: core_io.resp_data = {24'b0, mem_io.resp_data[offset*8+:8]};
                         endcase
+                    end
+                end
+            end
+            POSTMISS: begin
+                addr_to_cache = next_req.addr;
+                if (is_hit) begin
+                    core_io.resp_valid = 1;
+                    core_io.resp_data  = data_from_cache;
+                    if (next_req.write) begin
+                        write_to_cache = 1;
+                        write_data_to_cache = {96'b0, next_req.data};
+                    end
+                end else begin
+                    if (is_dirty) begin
+                        mem_io.req_valid = 1;
+                        mem_io.req_addr = {next_req.addr[ADDR_WIDTH-1:M], {M{1'b0}}};
+                        mem_io.req_write_en = 1;
+                        mem_io.req_data = data_from_cache;
                     end else begin
-                        if (stage_io.req_write_en) begin
-                            pending_write.valid <= stage_io.req_valid;
-                            pending_write.addr <= stage_io.req_addr;
-                            pending_write.data <= stage_io.req_data;
-                            pending_write.write_en <= stage_io.req_write_en;
-                            pending_write.kind <= stage_io.req_type;
-                        end else if (line[idx].valid && line[idx].dirty) begin
-                            pending_read.valid <= !stage_io.req_write_en;
-                            pending_read.addr  <= stage_io.req_addr;
-                        end
+                        mem_io.req_valid = 1;
+                        mem_io.req_addr = {next_req.addr[ADDR_WIDTH-1:M], {M{1'b0}}};
+                        mem_io.req_write_en = 0;
                     end
                 end
-                MISS: begin
-                    mem_io.req_valid <= 1;
-                    if (line[idx].valid && line[idx].dirty) begin : WRITE_BACK
-                        pending_read.valid  <= !stage_io.req_write_en;
-                        pending_read.addr   <= stage_io.req_addr;
-                        mem_io.req_addr     <= {line[idx].tag, idx, {M{1'b0}}};
-                        mem_io.req_write_en <= 1;
-                        mem_io.req_data     <= line[idx].data;
-                    end else begin
-                        mem_io.req_addr <= {tag, idx, {M{1'b0}}};
-                        line[idx].tag   <= stage_io.req_addr[PHY_ADDR_LEN-1:N];
-                    end
-                end
-                WRITEBACK: begin
-                    mem_io.req_valid <= pending_read.valid;
-                    mem_io.req_addr <= pending_read.addr;
-                    mem_io.req_write_en <= 0;
-                    pending_read <= '0;
-                end
-                WAIT: begin
-                    if (mem_io.resp_valid) begin
-                        line[idx].valid <= 1;
-                        line[idx].dirty <= 0;
-                        line[idx].data <= mem_io.resp_data;
-                        stage_io.resp_data <= mem_io.resp_data[offset*8+:XLEN];
-                        if (!pending_write.valid) begin
-                            stage_io.resp_valid <= 1;
-                        end
-                    end
-                end
-                WRITE: begin
-                    stage_io.resp_valid <= 1;
-                    pending_write <= '0;
-                    line[pending_write.addr[N-1:M]].dirty <= 1;
-                    unique case (pending_write.kind)
-                        WORD:
-                        line[pending_write.addr[N-1:M]].data[pending_write.addr[M-1:0]*8+:32] <= pending_write.data;
-                        HALF, UHALF:
-                        line[pending_write.addr[N-1:M]].data[pending_write.addr[M-1:0]*8+:16] <= pending_write.data[15:0];
-                        BYTE, UBYTE:
-                        line[pending_write.addr[N-1:M]].data[pending_write.addr[M-1:0]*8+:8] <= pending_write.data[7:0];
-                    endcase
-                end
-            endcase
-        end
+            end
+            WRITEBACK: begin
+                addr_to_cache = pending_req.addr;
+                mem_io.req_valid = 1;
+                mem_io.req_addr = {pending_req.addr[ADDR_WIDTH-1:M], {M{1'b0}}};
+                mem_io.req_write_en = 0;
+            end
+            WRITE: begin
+                addr_to_cache = pending_req.addr;
+                core_io.resp_valid = 1;
+                write_to_cache = 1;
+                write_data_to_cache = {96'b0, pending_req.data};
+            end
+        endcase
     end
 
 endmodule
---
+++ rtl/cache_data.sv
@@ -0,0 +1,72 @@
+module cache_data
+    import riscv_pkg::*;
+    import pa_pkg::*;
+#(
+    parameter integer unsigned LINE_LEN   = CACHE_LINE_LEN,
+    parameter integer unsigned NUM_SETS   = 4,
+    parameter integer unsigned ADDR_WIDTH = PHY_ADDR_LEN,
+
+    localparam integer unsigned M = $clog2(LINE_LEN / 8),
+    localparam integer unsigned N = $clog2(NUM_SETS) + M
+) (
+    input logic clk_i,
+    input logic rst_i,
+
+    input logic [ADDR_WIDTH-1:0] addr_i,
+    input logic write_i,  // write enable byte, half or word to cache
+    input logic write_line_i,  // write enable line to cache
+    input logic [LINE_LEN-1:0] write_data_i,  // data to be written
+    input mem_width_t kind_i,  // byte, half or word access
+
+    output logic hit_o,
+    output logic dirty_o,
+    output logic [XLEN-1:0] data_o
+);
+    typedef struct packed {
+        logic valid, dirty;
+        logic [PHY_ADDR_LEN-N-1:0] tag;
+        logic [LINE_LEN-1:0] data;
+    } line_t;
+
+    wire [ADDR_WIDTH-N-1:0] tag;
+    wire [N-M-1:0] idx;
+    wire [M-1:0] offset;
+    line_t [NUM_SETS-1:0] lines;
+
+    assign tag = addr_i[ADDR_WIDTH-1:N];
+    assign idx = addr_i[N-1:M];
+    assign offset = addr_i[M-1:0];
+
+    assign hit_o = lines[idx].tag == tag && lines[idx].valid;
+    assign dirty_o = lines[idx].dirty;
+
+    always_comb begin
+        data_o = '0;
+        unique case (kind_i)
+            BYTE, UBYTE: data_o = {24'b0, lines[idx].data[offset*8+:8]};
+            HALF, UHALF: data_o = {16'b0, lines[idx].data[offset*8+:16]};
+            WORD: data_o = {lines[idx].data[offset*8+:32]};
+        endcase
+    end
+
+    always_ff @(posedge clk_i, posedge rst_i) begin
+        if (rst_i) begin
+            lines <= '0;
+        end else begin
+            if (write_i) begin
+                lines[idx].dirty <= 1;
+                lines[idx].valid <= 1;
+                unique case (kind_i)
+                    BYTE, UBYTE: lines[idx].data[offset*8+:8] <= write_data_i[7:0];
+                    HALF, UHALF: lines[idx].data[offset*8+:16] <= write_data_i[15:0];
+                    WORD: lines[idx].data[offset*8+:32] <= write_data_i[31:0];
+                endcase
+            end else if (write_line_i) begin
+                lines[idx].valid <= 1;
+                lines[idx].dirty <= 0;
+                lines[idx].tag   <= tag;
+                lines[idx].data  <= write_data_i;
+            end
+        end
+    end
+endmodule
